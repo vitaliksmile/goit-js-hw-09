af0fc76a9b4c36acacfba91ee2e51058ad23667c
vitaliksmile <vitaliksmile777@gmail.com>
2022-09-28 19:56:38 +0300

    01-color-switcher-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const refs = {
+  bodyPage: document.querySelector('body'),
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+};
+let startInterval = null;
 
+refs.buttonStart.addEventListener('click', () => {
+  refs.buttonStart.disabled = true;
+  refs.buttonStop.disabled = false;
+  startInterval = setInterval(() => {
+    refs.bodyPage.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+refs.buttonStop.addEventListener('click', () => {
+  refs.buttonStart.disabled = false;
+  refs.buttonStop.disabled = true;
+  clearInterval(startInterval);
+});
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
