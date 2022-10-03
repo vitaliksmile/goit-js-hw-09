be5411adc2467b08ffd0d4c9111e3fa0555913af
vitaliksmile <vitaliksmile777@gmail.com>
2022-10-03 17:18:41 +0300

    03-promises-done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
+const { Notify } = require('notiflix');
+
+const refs = {
+  formRef: document.querySelector('.form'),
+  btnRef: document.querySelector('button'),
+};
+
+refs.formRef.addEventListener('submit', onHanddleSubmit);
+
+function onHanddleSubmit(event) {
+  console.log(1);
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let dalayHandle = Number(delay.value);
+
+  let stepHandle = Number(step.value);
+
+  let amountHandle = Number(amount.value);
+
+  for (let i = 1; i <= amountHandle; i += 1) {
+    createPromise(i, dalayHandle)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    dalayHandle += stepHandle;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
